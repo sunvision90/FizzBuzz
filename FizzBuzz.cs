c6d635763f316d6ca85528db10a12d50d3ba9810
Mikolaj <sunvision90@gmail.com>
2024-05-13 14:11:43 +0200

    Add project files.

---
+++ FizzBuzz.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class FizzBuzz
+    {
+       public string Play(int number)
+        {
+            if (number % 3 == 0 || number % 5 == 0)
+            
+                return "FizzBuzz";
+                        
+            if (number % 3 == 0)
+                return "Fizz";
+            
+            if ((number % 5) == 0) 
+                return "Buzz";
+
+            else
+                return "Liczba nie jest podzielna ani przez 3 ani przez 5. Sprobuj ponownie";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzz fizzBuzz = new FizzBuzz();
+
+            while (true)
+            {
+                Console.WriteLine("Podaj proszę liczbę. Gdy bedzie podzielna przez 3 lub 5 lub oba pojawi się stosowny komunikat");
+
+                var number = GetNumber();
+
+                Console.WriteLine(fizzBuzz.Play(number));
+            }
+
+            
+
+        }
+
+        private static int GetNumber()
+        {
+            while (true)
+            {
+                if (!int.TryParse(Console.ReadLine(), out var number))
+                {
+                    Console.WriteLine("Podałeś nieprawidłowe dane");
+                    continue;
+                }
+                {
+                    return number;
+                }
+            }
+        }
+    }
+}
